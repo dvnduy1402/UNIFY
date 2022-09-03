93d1dfc535cce8e0b111706c3f5b44fc13290a59
dvnduy1402 <dvnduy1402@gmail.com>
2022-09-03 14:12:42 +0700

    app color and app Text

--- food_app_demo/lib/home/home_page.dart
+++ food_app_demo/lib/home/home_page.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
-import 'header_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,7 +12,36 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Home Page',
-      home: Scaffold(body: SafeArea(child: HeaderPage(),))
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(
+            margin: const EdgeInsets.only(top: 10, bottom: 5),
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: const [Text('Contry'), Text('region')],
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                        color: Colors.blue[400],
+                      ),
+                      child: IconButton(
+                        icon: const Icon(Icons.search, color: Colors.white),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- food_app_demo/lib/main.dart
+++ food_app_demo/lib/main.dart
@@ -1,7 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:food_app_demo/home/home_page.dart';
 
-import 'home_page/home_page.dart';
 
 void main(List<String> args) {
   runApp(const HomePage());
