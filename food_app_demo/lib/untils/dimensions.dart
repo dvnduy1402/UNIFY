8462decc032a9972fa2466228b46cfdf6bdb975a
dvnduy1402 <dvnduy1402@gmail.com>
2022-09-10 13:51:56 +0700

    create demensions file

--- food_app_demo/lib/untils/dimensions.dart
+++ food_app_demo/lib/untils/dimensions.dart
@@ -1,3 +1,8 @@
-class Dimensions{
+import 'package:get/get.dart';
 
+class Dimensions{
+  static double screenHeight = Get.context!.height;
+  static double screenWidth = Get.context!.width;
+
+  static double pageView = screenHeight;
 }
